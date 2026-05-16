b05809fe1d903b15f810a2ee71922a666dcf414e
João Pedro da Silva Nascimento <joaopedronascimento171@gmail.com>
2026-05-15 22:06:44 -0300

    Lista de planos de aula criado no user

--- mpc/modules/user.py
+++ mpc/modules/user.py
@@ -1,7 +1,8 @@
 class User():
-    def __init__(self, user_id: int, username: str):
+    def __init__(self, user_id: int, username: str, plan_ids: list[int]):
         self.user_id = user_id
         self.username = username
+        self.plan_ids = plan_ids
 
     def __str__(self):
         return f"User(id={self.user_id}, username='{self.username}')"
